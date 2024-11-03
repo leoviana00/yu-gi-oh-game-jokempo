36061a8ae3878e867d41402c338b598fa073467b
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2024-11-02 21:08:23 -0300

    feat(state): state define

---
+++ src/scripts/engine.js
@@ -0,0 +1,33 @@
+// States define
+const state = {
+    score:{
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites:{
+        avatar: document.getElementById("card-image"),
+        avatar: document.getElementById("card-name"),
+        avatar: document.getElementById("card-type"),
+    },
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        player: document.getElementById("computer-field-card"),
+    },
+    action: {
+        button: document.getElementById("next-duel"),
+    },
+};
+
+// const players = {
+//     player1: "player-cards",
+// }
+
+
+// Main function
+function init(){
+
+}
+
+// Call main function
+init();
